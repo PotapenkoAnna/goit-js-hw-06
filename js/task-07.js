e22e98c773cded306cd63d3a1738b188cbcced04
Potapenko Anna <potapka23@gmail.com>
2023-07-19 18:58:48 +0300

    task-07 is completed

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const inputFontSize = document.getElementById('font-size-control');
+const text = document.getElementById('text');
+
+inputFontSize.addEventListener('input', event => {
+    text.style.fontSize = `${event.target.value}px`
+})
